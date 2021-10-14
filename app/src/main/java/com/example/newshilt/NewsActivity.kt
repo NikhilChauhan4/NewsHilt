fc805801f41cb33651d6c6a855dd81401b877fb3
Nikhil Chauhan <nikhil.chauhan@mutualmobile.com>
2021-10-14 15:11:47 +0530

    refactor to data binding

--- app/src/main/java/com/example/newshilt/NewsActivity.kt
+++ app/src/main/java/com/example/newshilt/NewsActivity.kt
@@ -3,11 +3,13 @@ package com.example.newshilt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newshilt.data.Article
+import com.example.newshilt.databinding.ActivityNewsBinding
 import com.example.newshilt.recyclerview.NewsAdapter
 import com.example.newshilt.viewmodel.NewsViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,23 +18,23 @@ import dagger.hilt.android.AndroidEntryPoint
 class NewsActivity : AppCompatActivity() {
     private lateinit var adapter: NewsAdapter
     private var newsList: ArrayList<Article> = ArrayList()
-    lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerView: RecyclerView
+    lateinit var newsActivityBinding: ActivityNewsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val newsViewModel = ViewModelProvider(this).get(NewsViewModel::class.java)
-        setContentView(R.layout.activity_news)
-
-        recyclerView = findViewById(R.id.recycler_view)
+        newsActivityBinding = DataBindingUtil.setContentView(this, R.layout.activity_news)
+        recyclerView = newsActivityBinding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(this)
-        adapter = NewsAdapter(newsList)
+        adapter = NewsAdapter()
         recyclerView.adapter = adapter
 
         newsViewModel.newsItems
             .observe(this, Observer {
                 Log.d("TAG_NEWS_ACTIVITY", "onCreate: " + it.size)
                 newsList.addAll(it)
+                adapter.setNewsItemList(newsList)
                 adapter.notifyDataSetChanged()
             })
-
     }
 }
--- app/src/main/java/com/example/newshilt/recyclerview/NewsAdapter.kt
+++ app/src/main/java/com/example/newshilt/recyclerview/NewsAdapter.kt
@@ -1,49 +1,62 @@
 package com.example.newshilt.recyclerview
 
+import android.content.ClipData
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.newshilt.R
 import com.example.newshilt.data.Article
+import com.example.newshilt.databinding.NewsItemBinding
 import javax.inject.Inject
 import kotlin.math.log
 
-class NewsAdapter @Inject constructor(private val newsItemList: List<Article>) :
+class NewsAdapter @Inject constructor() :
     RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
+    private var newsItemList = ArrayList<Article>()
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val source: TextView = itemView.findViewById(R.id.source)
-        val title: TextView = itemView.findViewById(R.id.title)
-        val description: TextView = itemView.findViewById(R.id.description)
-        val publishedAt: TextView = itemView.findViewById(R.id.published_at)
-        val author: TextView = itemView.findViewById(R.id.author)
-        val newsImage: ImageView = itemView.findViewById(R.id.news_image)
+    inner class ViewHolder constructor(private val binding: NewsItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
+        fun bind(article: Article) {
+            binding.source.text = article.source.name
+            binding.title.text = article.title
+            binding.description.text = article.description
+            binding.publishedAt.text = article.publishedAt
+            binding.author.text = article.author
+//        holder.author.text = article.content
+            Log.d("image url", "onBindViewHolder: " + article.urlToImage)
+            Glide.with(binding.root)
+                .load(article.urlToImage)
+                .centerCrop()
+                .into(binding.newsImage)
+            binding.executePendingBindings();
+
+        }
+    }
+
+    fun setNewsItemList(newsList: ArrayList<Article>) {
+        newsItemList = newsList
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.news_item, parent, false)
-        return ViewHolder(view)
+        val binding: NewsItemBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.news_item,
+            parent,
+            false
+        )
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val news = newsItemList[position]
-        holder.source.text = news.source.name
-        holder.title.text = news.title
-        holder.description.text = news.description
-        holder.publishedAt.text = news.publishedAt
-        holder.author.text = news.author
-//        holder.author.text = news.content
-        Log.d("image url", "onBindViewHolder: " + news.urlToImage)
-        Glide.with(holder.itemView)
-            .load(news.urlToImage)
-            .centerCrop()
-            .into(holder.newsImage)
+        holder.bind(newsItemList[position])
     }
 
     override fun getItemCount() = newsItemList.size
